17ca663286a98d472b0ab5d4ba0055d8475c0f62
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-09 18:46:43 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,61 @@
-# Write your code here.
+def dictionary
+hash = {
+    "Hello" => "hi",
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+}
+end
+
+ def word_substituter (str)
+  array = str.split(" ")
+  array.each_with_index do |words,index|
+    dictionary.keys.each do |x|
+      if x == words
+      array[index] = dictionary[x]
+    end
+      end
+        end
+array.join(" ")
+end
+
+def bulk_tweet_shortener (arr)
+      arr.each_with_index do |tweet, index|
+      tweet_array = tweet.split(" ")
+      tweet_array.each_with_index do |word, index|
+      dictionary.keys.each do |x|
+      if x == word.downcase
+        tweet_array[index] = dictionary[x]
+      end
+    end
+  end
+  tweet = tweet_array.join(" ")
+  puts tweet
+
+    end
+end
+
+
+  def selective_tweet_shortener (tweet)
+if tweet.length > 140
+  word_substituter(tweet)
+else
+ tweet
+  end
+end
+
+
+def shortened_tweet_truncator (tweet)
+    if tweet.length > 140
+      tweet =  word_substituter(tweet)
+      tweet[0..136] + "..."
+    else
+      tweet
+  end
+end
